9c48a2b4656efe06567f7e39f4c8df3a95643786
sheetalsheriwala <sheetalsheriwala@gmail.com>
2023-02-24 12:49:58 +0530

    added some changes in files

---
+++ app/services/tilte.rb
@@ -0,0 +1,13 @@
+working on git :
+ 
+git branch
+git checkout -b "branch name" # create new branch
+git status
+git add : for adding new files
+git commit -m "[] added new files"
+git push origin branch name
+git pull origin branch name #if we want to sync code from current branch
+git checkout "branch name" #i we want to switch 
+git checkout child branch name #sync code from current branch
+git rebase parent_branch _name
+git cherry- pick shalID #take any commit from other branch 
